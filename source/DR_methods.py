c5110ef73c2ca5dae0c373577e30fd0903f5f108
Blake Nassar <bnassa1@lsu.edu>
2023-03-07 12:38:11 -0600

    Messing around to make studd work on the DR side

--- source/DR_methods.py
+++ source/DR_methods.py
@@ -1,4 +1,8 @@
 from sklearn.decomposition import PCA
-class PCA():
+class DR():
     def __init__(self):
         pass
+
+    def dPCA(self, n, data):
+        DPCA  = PCA(n_components = n)
+        return DPCA.fit_transform(data)
