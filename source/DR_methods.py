d7a8e71db3cf4c858eaaa60305551a21e98cbd90
Blake Nassar <bnassa1@lsu.edu>
2023-03-07 18:52:10 -0600

    Added tSNE. not sure if any of this is right, tho

--- source/DR_methods.py
+++ source/DR_methods.py
@@ -1,4 +1,5 @@
 from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
 class DR():
     def __init__(self):
         pass
@@ -6,3 +7,7 @@ class DR():
     def dPCA(self, n, data):
         DPCA  = PCA(n_components = n)
         return DPCA.fit_transform(data)
+    
+    def dtSNE(self, n, data):
+        dtSNE = TSNE(n_components= n, random_state= 1)
+        return dtSNE.fit_transform(data)
